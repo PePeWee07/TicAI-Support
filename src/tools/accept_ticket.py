ee5a9b25c4ef1a95864e354288c968e30d648297
Pepe <pepewee07@gmail.com>
2025-10-31 10:34:17 -0500

    modifacion de aceptacion de ticket

--- src/tools/accept_ticket.py
+++ src/tools/accept_ticket.py
@@ -3,42 +3,29 @@ from tools.config.registry import register_function, requires_roles
 from config.logging_config import logger
 import os
 
-@requires_roles("accept_or_reject_ticket", ["DOCENTE", "ADMINISTRATIVO", "ENCARGATURA"])
-@register_function("accept_or_reject_ticket")
-def accept_or_reject_ticket(**kwargs):
+@requires_roles("accept_ticket", ["DOCENTE", "ADMINISTRATIVO", "ENCARGATURA"])
+@register_function("accept_ticket")
+def accept_ticket(**kwargs):
     """
     Tomar una decisión sobre la solución dada de un caso.
 
     Parámetros esperados:
     - ticket_id: ID del Ticket
-    - status: 'aceptada' o 'rechazada'
-    - rejection_reason: motivo del rechazo (solo si es rechazada)
 
     Datos del usuario:
     - phone
     """
     ticket_id = kwargs.get("ticket_id")
     phone = kwargs.get("phone")
-    status = kwargs.get("status")
-    rejection_reason = kwargs.get("rejection_reason")
-
-    # Convertimos el texto a booleano
-    accepted = status == "aceptada"
-
-    payload = {
-        "accepted": accepted,
-        "ticketId": ticket_id,
-        "content": rejection_reason if not accepted else ""
-    }
 
     url = os.getenv("URL_BACKEND") + "v1/whatsapp/user/ticket/solution/decision"
-    params = {"whatsappPhone": phone}
+    params = {"whatsappPhone": phone, "ticketId": ticket_id}
     headers = {
         os.getenv("BACKEND_HEADER"): os.getenv("API_KEY_BACKEND")
     }
 
     try:
-        response = requests.post(url, json=payload, params=params, headers=headers)
+        response = requests.post(url, params=params, headers=headers)
         response.raise_for_status()
         data = response.json()
         return data.get("message") or data.get("error")
@@ -52,5 +39,5 @@ def accept_or_reject_ticket(**kwargs):
                 error_message = str(ex)
         else:
             error_message = str(ex)
-        logger.error(f"Error al registrar la decisión del ticket {ticket_id}: {error_message}")
-        return f"No se pudo registrar la decisión sobre el ticket #{ticket_id}: {error_message}"
+        logger.error(f"Error al aceptar la solución del ticket {ticket_id}: {error_message}")
+        return f"No se pudo aceptar la solución del ticket #{ticket_id}: {error_message}"
